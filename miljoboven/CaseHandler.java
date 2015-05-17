27e4328368cac2bd72679d34beb2382e401c860c
jmalmer <jmalmer@telia.com>
2015-05-17 15:45:36 +0200

    Lagt till metod addCase

--- miljoboven/CaseHandler.java
+++ miljoboven/CaseHandler.java
@@ -13,6 +13,7 @@ public class CaseHandler {
     private DBHandler dbHandler;
     private NotificationHandler notificationHandler;
     private User user;
+    private Case currentCase;
 
     public CaseHandler() {
 
@@ -46,7 +47,34 @@ public class CaseHandler {
         return user;
     }
 
-    public startPageUI login(String username, char[] password) {
+    /**
+     * Creates a new Case-object and adds it to the dbHandler.
+     * If not logged in no object is created.
+     * @param location Location of the violation
+     * @param violationType Type of violation
+     * @param date Date when the violation was found
+     * @param citizen Name of citizen that has reported the violation
+     * @param misc Miscellaneous information
+     * @return caseID of new Case, 0 if no object was created
+     */
+    public int addCase(String location, String violationType, String date,
+            String citizen, String misc) {
+        int caseID = 0;
+        if (user != null) {
+            currentCase = new Case(user, location, violationType, date, citizen, misc);
+            caseID = currentCase.getCaseID();
+            dbHandler.addCase(currentCase);
+        }
+        return caseID;
+    }
+    
+    /**
+     * Log in user.
+     * @param username
+     * @param password
+     * @return TODO
+     */
+    public StartPageUI login(String username, char[] password) {
         user = SecurityCheck.verifyLogin(username, password);
         return null; // TODO
     }
