4626aef40134e0d2891dad719b25abfab1f2ff43
jmalmer <jmalmer@telia.com>
2015-05-17 15:35:34 +0200

    Lagt till metod addCase

--- miljoboven/DBHandler.java
+++ miljoboven/DBHandler.java
@@ -1,10 +1,31 @@
 package miljoboven;
 
+import java.util.Map;
+import java.util.HashMap;
+
 /**
  *
  * @author johan
  */
-class DBHandler {
+public class DBHandler {
     
+    Map<Integer, Case> cases;
+
+    public DBHandler() {
+        this.cases = new HashMap<>();
+    }
+    
+    /**
+     * Adds new Case to the db.
+     * @param newCase Case to be added
+     * @return true if success
+     */
+    public boolean addCase(Case newCase) {
+        boolean success = !(cases.containsKey(newCase.getCaseID()));
+        if (success) {
+            cases.put(newCase.getCaseID(), newCase);
+        }
+        return success;
+    }
     
 }
