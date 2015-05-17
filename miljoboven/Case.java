f34be43c1aaf842470f4eea215a9cd21fb4a38b6
jmalmer <jmalmer@telia.com>
2015-05-17 15:36:47 +0200

    Lagt till konstruktor och getters

--- miljoboven/Case.java
+++ miljoboven/Case.java
@@ -13,7 +13,56 @@ class Case {
     private String violationType;
     private String date;
     private String citizen;
-    private String miscellaneous;
+    private String misc;
+    
+    private static int nextCaseID = 1;
+
+    public Case(User caseWorker, String location, String violationType, String date, String citizen, String misc) {
+        this.caseWorker = caseWorker;
+        this.location = location;
+        this.violationType = violationType;
+        this.date = date;
+        this.citizen = citizen;
+        this.misc = misc;
+        this.caseID = nextCaseID ++;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public int getCaseID() {
+        return caseID;
+    }
+
+    public User getCaseWorker() {
+        return caseWorker;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public String getViolationType() {
+        return violationType;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getCitizen() {
+        return citizen;
+    }
+
+    public String getMisc() {
+        return misc;
+    }
+
+    public static int getNextCaseID() {
+        return nextCaseID;
+    }
+    
     
     
 }
